0fbc4e6673c21227b60f030723fe1638659c9b25
Sergey <Sergey@yqndex.ru>
2023-02-13 22:58:51 +0300

    all

---
+++ task_25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать библиотеку Math!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int construction(int a, int b)
+{
+  int result = 1;
+  for (int count = 1; count <= b; count++) 
+  {
+    result = result * a;
+  }
+  return result;
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень: ");
+int b = int.Parse(Console.ReadLine()!);
+int pow = construction(a, b);
+Console.Write(pow);
---
+++ task_27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void Dellet(String[] args)
+{
+  Console.WriteLine("Введите число: ");
+  int a = int.Parse(Console.ReadLine()!);
+  int sum = 0;
+  int n = 0;
+  {
+    while (a > 0)
+    {
+      n = a % 10;
+      sum = sum + n;
+      a = a / 10;
+    }
+    Console.WriteLine($"Сумма числа: {sum}");
+  }
+}
+Dellet(args);
